0822b88bfb5adcc8d3e4df47b274c1e5a1f7ef91
iamyck <949735538@qq.com>
2015-01-23 19:57:44 +0800

    version 6.0

--- file-transfer.h
+++ file-transfer.h
@@ -1,3 +1,14 @@
+/*****************************************************************************/
+/* Copyright (C) SSE-USTC, 2014-2015                                         */
+/*                                                                           */
+/*  FILE NAME             :  file-transfer.h                                 */
+/*  PRINCIPAL AUTHOR      :  SA14226208                                      */
+/*  LANGUAGE              :  C                                               */
+/*  TARGET ENVIRONMENT    :  ANY                                             */
+/*  DATE OF FIRST RELEASE :  2015/01/21                                      */
+/*  DESCRIPTION           :  file-transfer.h                                 */
+/*****************************************************************************/
+
 #ifndef FILE_TRANSFER_H
 #define FILE_TRANSFER_H
 
@@ -9,37 +20,44 @@
 #include<string.h>
 #include"file-stream.h"
 
+#define THREAD_NUM          1     /* tcp thread num */
+#define TCP_DATAGRAM_SIZE   8192  /* tcp data size per package */
+#define TCP_BLOCK_SIZE      128   /* tcp package num in menory buf */
+#define UDP_DATAGRAM_SIZE   2048  /* udp data size per package */
+#define UDP_BLOCK_SIZE      256   /* udp package num in menory buf */
+
 /* control info */
-struct controlgram
+typedef struct file_block_info
 {
-	int 	begin;
-	int 	size;
-	unsigned char 	map[256];
-};
+    int            seek;          /* start point in file */
+    int            size;          /* data size of this block */
+}File_block;
 
 /* data info */
 struct datagram
 {
-	int 	begin;
-	int 	index;
-	char 	buf[1024];	
+    int            index;         /* package id */
+    char           buf[1024];	  /* real data */
 };
 
 /* thread args */
-struct thread_args
+typedef struct thread_args
 {
-	char 	*filename;	
-	int 	seek;
-	int 	size;
-	int 	fd;
-};
+    char           *filename;     /* filename */	
+    int            seek;          /* start point */
+    int            size;          /* data size */
+    int            fd;            /* socket connnetion fd */
+}Thread_args;
 
-void tcp_file_send(int, int, char *);
+void tcp_file_send(int controlfd, /* tcp connnetion fd */
+                   int serverfd); /* tcp server fd */
 
-void tcp_file_get(int);
+void tcp_file_get(int controlfd,  /* tcp connnetion fd */
+                  char *filename);/* filename */
 
-void udp_file_send(int , char *);
+void udp_file_send(int controlfd);/* tcp connnetion fd */
 
-void udp_file_get(int);
+void udp_file_get(int controlfd,  /* tcp connnetion fd */
+                  char *filename);/* filename */
 
 #endif /* file-transfer.h */
